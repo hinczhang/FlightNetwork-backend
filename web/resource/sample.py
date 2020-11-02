05ae89a098291b7fdae3217872f346bf83186d8a
张炅焱 <70082542+hinczhang@users.noreply.github.com>
2020-11-02 15:54:55 +0800

    change web

--- web/resource/sample.py
+++ web/resource/sample.py
@@ -10,7 +10,13 @@ class Sample(Resource):
 	def post(self):#open post method
 		print(json.loads(request.data)['mode'])#the data delivered from frontend to backend are stored in the request.data, which should be jsonfied.
 		data=pd.read_csv("20200101.csv")
-		data=list(data['firstseen'])
-		data = json.dumps({'doc':data})
+		orgX=list(data['org_latitude_deg'])
+		orgY=list(data['org_longitude_deg'])
+		dstX=list(data['dst_latitude_deg'])
+		dstY=list(data['dst_longitude_deg'])
+		routes=[]
+		for i in range(len(orgX)):
+			routes.append([[orgY[i],orgX[i],0],[dstY[i],dstX[i],0]])
+		data = json.dumps({'routes':routes})
 		res = Response(response=data, status=200, mimetype="application/json")#send message to frontend
 		return res
