5ea058cf66e74e3748abbcbd6f173fb08cf3cdb6
张炅焱 <70082542+hinczhang@users.noreply.github.com>
2020-11-04 17:50:27 +0800

    second change

--- web/resource/sample.py
+++ web/resource/sample.py
@@ -3,12 +3,34 @@ from flask import Response,request
 import json
 import pandas as pd
 
+
+date_list=list(range(20200101,20200132))
+date_list+=list(range(20200201,20200230))
+date_list+=list(range(20200301,20200332))
+date_list+=list(range(20200401,20200431))
 #return useful information
 class Sample(Resource):
 	def get(self):#block get method
 		pass
 	def post(self):#open post method
-		print(json.loads(request.data)['mode'])#the data delivered from frontend to backend are stored in the request.data, which should be jsonfied.
+		flag=json.loads(request.data)['mode']
+		print(flag)#the data delivered from frontend to backend are stored in the request.data, which should be jsonfied.
+		data=None
+		if flag==1:
+			data = json.dumps({'date_list':date_list})
+		elif flag==2:
+			date=json.loads(request.data)['date']
+			csv=pd.read_csv("./flight/"+str(date)+".csv")
+			orgX=list(csv['org_latitude_deg'])
+			orgY=list(csv['org_longitude_deg'])
+			dstX=list(csv['dst_latitude_deg'])
+			dstY=list(csv['dst_longitude_deg'])
+			routes=[]
+			for i in range(len(orgX)):
+				routes.append([[orgY[i],orgX[i],0],[dstY[i],dstX[i],0]])
+			data = json.dumps({'routes':routes})
+
+		'''
 		data=pd.read_csv("20200101.csv")
 		orgX=list(data['org_latitude_deg'])
 		orgY=list(data['org_longitude_deg'])
@@ -18,5 +40,6 @@ class Sample(Resource):
 		for i in range(len(orgX)):
 			routes.append([[orgY[i],orgX[i],0],[dstY[i],dstX[i],0]])
 		data = json.dumps({'routes':routes})
+		'''
 		res = Response(response=data, status=200, mimetype="application/json")#send message to frontend
 		return res
